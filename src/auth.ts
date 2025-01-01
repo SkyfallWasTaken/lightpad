a29202cb9e12f95a7d7cec5b43b5e050feba6d9b
Mahad Kalam <mahadkalam1@proton.me>
2025-01-01 07:25:34 +0000

    Always set trustHost to true

--- src/auth.ts
+++ src/auth.ts
@@ -3,5 +3,7 @@ import GitHub from "@auth/sveltekit/providers/github"
 
 export const { handle } = SvelteKitAuth({
     providers: [GitHub],
-    trustHost: Boolean(import.meta.env.AUTH_TRUST_HOST)
+    // Auth.js docs say that this should be set to true. But for some reason, it isn't!
+    // The Discord server shows several people using SvelteKit facing this issue.
+    trustHost: true
 })
