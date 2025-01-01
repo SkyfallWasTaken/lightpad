d79cc36a727c3b58e8a825203c90d9600f940650
Mahad Kalam <mahadkalam1@proton.me>
2025-01-01 06:39:51 +0000

    Use auth tokens for Turso

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,5 +3,5 @@ import { createClient } from '@libsql/client';
 import { env } from '$env/dynamic/private';
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = createClient({ url: env.DATABASE_URL });
+const client = createClient({ url: env.DATABASE_URL, authToken: env.DATABASE_AUTH_TOKEN });
 export const db = drizzle(client);
