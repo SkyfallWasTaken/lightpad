512c94fa0ff3490f8fd15e6941b7c20a24b31819
Mahad Kalam <mahadkalam1@proton.me>
2025-01-01 07:22:03 +0000

    trustHost when env set

--- src/auth.ts
+++ src/auth.ts
@@ -3,4 +3,5 @@ import GitHub from "@auth/sveltekit/providers/github"
 
 export const { handle } = SvelteKitAuth({
     providers: [GitHub],
+    trustHost: Boolean(import.meta.env.AUTH_TRUST_HOST)
 })
